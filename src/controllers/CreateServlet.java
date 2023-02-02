a1070e210d5812e6ce78c9a8b31b6e86e6c68726
user <user@DESKTOP-Q3TVT9P>
2023-02-02 09:35:29 +0900

    フラッシュメッセージを追加の修正

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -50,6 +50,7 @@ public class CreateServlet extends HttpServlet {
 
             // データベースに保存
             em.persist(m);
+            em.getTransaction().commit();
             request.getSession().setAttribute("flush", "登録が完了しました。");       // ここを追記
             em.close();
 
